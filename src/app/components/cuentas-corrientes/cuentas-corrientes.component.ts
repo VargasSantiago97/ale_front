ea09603b57c13f638bf8995a3a7e0e0a524c2b8e
Santiago <vargassantiago97@gmail.com>
2023-04-21 12:47:23 -0300

    cuentas

--- src/app/components/cuentas-corrientes/cuentas-corrientes.component.ts
+++ src/app/components/cuentas-corrientes/cuentas-corrientes.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { ComunicacionService } from 'src/app/services/comunicacion.service';
-import { PadronService } from 'src/app/services/padron.service';
+
+declare var vars: any;
+
 
 @Component({
     selector: 'app-cuentas-corrientes',
@@ -10,12 +12,21 @@ import { PadronService } from 'src/app/services/padron.service';
 })
 export class CuentasCorrientesComponent {
 
+    API_URI = vars.API_URI_CONAGRO;
 
     db_camiones: any = []
     db_choferes: any = []
     db_transportistas: any = []
     db_condicion_iva: any = []
     db_socios: any = []
+    db_movimientos: any = []
+    db_granos: any = []
+    db_ordenes_carga: any = []
+
+    dataMovimientosASeleccionar: any = []
+    dataMovimientosSeleccionados: any = []
+
+    datosAsiento: any = {}
 
     transportista: any;
     socio: any;
@@ -29,6 +40,14 @@ export class CuentasCorrientesComponent {
     load_choferes: any = true
     load_transportistas: any = true
     load_condicion_iva: any = true
+    load_movimientos: any = true
+    load_granos: any = true
+    load_ordenes_carga: any = true
+
+    displayNuevoViaje: any = false
+
+    cols: any = []
+    selectedColumns: any = []
 
     constructor(
         private messageService: MessageService,
@@ -36,11 +55,47 @@ export class CuentasCorrientesComponent {
     ){}
 
     ngOnInit(){
+        this.cols = [
+            { field: "fecha", header: "Fecha" },
+            { field: "cultivo", header: "Cultivo" },
+            { field: "orden", header: "O.C." },
+            { field: "ctg", header: "C.T.G." },
+            { field: "cpe", header: "N° C.P." },
+
+            { field: "campo", header: "Est." },
+            { field: "pat", header: "Pats." },
+
+            { field: "kg_neto_final", header: "Neto Final" },
+
+            { field: "tarifa", header: "Tarifa" },
+            { field: "kg_descarga", header: "Kg Descargados" },
+            { field: "monto_final", header: "Monto calc" },
+        ];
+        this.selectedColumns = [
+            { field: "fecha", header: "Fecha" },
+            { field: "cultivo", header: "Cultivo" },
+            { field: "orden", header: "O.C." },
+            { field: "ctg", header: "C.T.G." },
+            { field: "cpe", header: "N° C.P." },
+
+            { field: "campo", header: "Est." },
+            { field: "pat", header: "Pats." },
+
+            { field: "kg_neto_final", header: "Neto Final" },
+
+            { field: "tarifa", header: "Tarifa" },
+            { field: "kg_descarga", header: "Kg Descargados" },
+            { field: "monto_final", header: "Monto calc" },
+        ];
+
         this.obtenerCamiones()
         this.obtenerChoferes()
         this.obtenerTransportistas()
         this.obtenerCondicion_iva()
         this.obtenerSocios()
+        this.obtenerMovimientos()
+        this.obtenerGranos()
+        this.obtenerOrdenesCarga()
     }
 
     //CONEXION A BASE DE DATOS
@@ -98,15 +153,121 @@ export class CuentasCorrientesComponent {
             }
         )
     }
-
-
+    obtenerMovimientos() {
+        this.comunicacionService.getDB('movimientos').subscribe(
+            (res: any) => {
+                this.db_movimientos = res;
+                this.load_movimientos = false;
+            },
+            (err: any) => {
+                console.log(err)
+            }
+        )
+    }
+    obtenerGranos() {
+        this.comunicacionService.getDB('granos').subscribe(
+            (res: any) => {
+                this.db_granos = res;
+                this.load_granos = false;
+            },
+            (err: any) => {
+                console.log(err)
+            }
+        )
+    }
+    obtenerOrdenesCarga(){
+        this.comunicacionService.getDB('orden_carga').subscribe(
+            (res: any) => {
+                this.db_ordenes_carga = res;
+                this.load_ordenes_carga = false;
+            },
+            (err: any) => {
+                console.log(err)
+            }
+        )
+    }
 
     onSelectTransporte(){
-
+        this.buscarDatosTransportista()
     }
 
     onSelectSocio(){
-        console.log('s')
+        if(this.transportista){
+            this.buscarDatosTransportista()
+        }
+    }
+
+    buscarDatosTransportista(){
+        this.dataMovimientosASeleccionar = []
+
+        this.db_movimientos.filter((e:any) => { return (( e.id_socio == this.socio.id) && (e.id_transporte==this.transportista.id)) }).map((e:any) => {
+
+            const dateObj = new Date(e.fecha);
+            const year = dateObj.getFullYear();
+            const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+            const day = dateObj.getDate().toString().padStart(2, '0');
+            const dateString = `${year}/${month}/${day}`;
+
+            const patentes = this.db_camiones.some((f:any) => {return f.id == e.id_camion}) ? this.db_camiones.find((f:any) => {return f.id == e.id_camion}).patente_chasis +' / '+ this.db_camiones.find((f:any) => {return f.id == e.id_camion}).patente_acoplado : ''
+
+            this.dataMovimientosASeleccionar.push({
+                id: e.id,
+                fecha : dateString,
+                cultivo : this.db_granos.some((f:any) => {return f.id == e.id_grano}) ? this.db_granos.find((f:any) => {return f.id == e.id_grano}).alias : '-',
+                orden : this.db_ordenes_carga.some((f:any) => {return f.id_movimiento == e.id}) ? this.db_ordenes_carga.find((f:any) => {return f.id_movimiento == e.id}).numero : '',
+                ctg : '10105050505',
+                cpe : '001-5555',
+                campo : e.campo,
+                pat : patentes,
+                kg_neto_final : e.kg_neto_final,
+                tarifa : 7000,
+                kg_descarga : 35.20,
+                monto_final : 246400
+            })
+        })
+    }
+
+    transformarDatoMostrarTabla(dato:any, tipo:any){
+        return dato
+    }
+
+    agregarMovimiento(mov:any){
+        this.dataMovimientosSeleccionados.push(mov)
+
+        var valorSuma = 0
+        this.dataMovimientosSeleccionados.forEach((e:any) => {
+            const valorActual = parseFloat(e.monto_final) ? parseFloat(e.monto_final) : 0
+            valorSuma = valorSuma + valorActual
+        });
+
+        this.datosAsiento.montoTotal = valorSuma.toFixed(2)
+        this.datosAsiento.montoFactura = valorSuma.toFixed(2)
+        this.datosAsiento.descripcion = 'FLETE'
+    }
+
+    onUpload(event: any) {
+        alert('Cargado 🫶')
+    }
+
+    verVars(){
+        console.log(this.datosAsiento)
+    }
+
+    nuevoAsientoViaje(){
+        this.displayNuevoViaje = true
+
+        const fecha = new Date().toISOString().slice(0, 10);
+
+        this.datosAsiento = {
+            montoTotal: 0,
+            letra: 'A',
+            punto: null,
+            numero: null,
+            montoFactura: 0,
+            fecha: fecha,
+            descripcion: null,
+            observaciones: null
+        }
     }
 
 }
