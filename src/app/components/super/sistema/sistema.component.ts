1da8b16fcde8d27260909401162b6a24710c1ea2
Santiago <vargassantiago97@gmail.com>
2023-06-22 20:40:01 -0300

    sistema

--- src/app/components/super/sistema/sistema.component.ts
+++ src/app/components/super/sistema/sistema.component.ts
@@ -14,6 +14,10 @@ export class SistemaComponent {
     db_locales: any = {}
 
 
+    filas: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+    columnas: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    fondos: any = {}
+
 
     varr: any = 0
     colsProd: any = []
@@ -39,6 +43,13 @@ export class SistemaComponent {
     ) {}
 
     ngOnInit() {
+        for (let index = 1; index <= this.filas.length; index++) {
+            this.fondos[index] = {}
+            for (let j = 1; j <= this.columnas.length; j++) {
+                this.fondos[index][j] = true
+            }
+        }
+
         this.colsProd = [
             {field: 'id', header: 'Id'},
             {field: 'socio_movimiento_id', header: 'Id Soc. Mov.'},
